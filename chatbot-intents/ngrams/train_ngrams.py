47b406222216859df2387023c27337f4fa158dbe
fabon <dzogang.fabon@gmail.com>
2020-07-14 18:29:48 +0100

    ngrams runs ready for paper

--- chatbot-intents/ngrams/train_ngrams.py
+++ chatbot-intents/ngrams/train_ngrams.py
@@ -74,29 +74,28 @@ def load_english_model(recompute_index, training_inds, max_f1, trial):
 
 def train_model(recompute_index, training_inds, language, trial):
 #     if not os.path.isfile(OUTPUT_DIR + language + "_index_ngrams.pkl") or not os.path.isfile(OUTPUT_DIR + language + "_X_train_ngrams.pkl"):
+    training_inds=None if len(training_inds)==1 else training_inds
     if recompute_index:
         index, word_counts, X_train, _, X_test, _=load_datasets_binary(language, training_inds)
-        y_train=pickle.load(open(  OUTPUT_DIR  + language + "_train_multiclass_labels.pkl", "rb"))
-        y_test=pickle.load(open(  OUTPUT_DIR  + language + "_test_multiclass_labels.pkl", "rb"))
-        pickle.dump(index, open(  OUTPUT_DIR  + language + "_index_ngrams_%i.pkl" % len(training_inds), "wb"))
-#         return None, None, None, None, None, None
+        y_train=pickle.load(open(  OUTPUT_DIR  + language + "_train_multiclass_labels_merged.pkl", "rb"))
+        y_test=pickle.load(open(  OUTPUT_DIR  + language + "_test_multiclass_labels_merged.pkl", "rb"))
+        pickle.dump(index, open(  OUTPUT_DIR  + language + "_index_ngrams_%i.pkl" % X_train.shape[0], "wb"))
         pickle.dump(X_train, open(OUTPUT_DIR  + language + "_X_train_ngrams.pkl", "wb"))
         pickle.dump(y_train, open(OUTPUT_DIR  + language + "_y_train_ngrams.pkl", "wb"))
         pickle.dump(X_test, open( OUTPUT_DIR  + language + "_X_test_ngrams.pkl", "wb"))
         pickle.dump(y_test, open( OUTPUT_DIR  + language + "_y_test_ngrams.pkl", "wb"))
         pickle.dump(word_counts, open(OUTPUT_DIR + language + "_wc.pkl", "wb"))
     else:
-        index=pickle.load(open(   OUTPUT_DIR  + language + "_index_ngrams_%i.pkl" % len(training_inds), "rb"))
         X_train=pickle.load(open( OUTPUT_DIR  + language + "_X_train_ngrams.pkl", "rb"))
-        y_train=pickle.load(open(  OUTPUT_DIR  + language + "_train_multiclass_labels.pkl", "rb"))
+        index=pickle.load(open(   OUTPUT_DIR  + language + "_index_ngrams_%i.pkl" % X_train.shape[0], "rb"))
+        y_train=pickle.load(open(  OUTPUT_DIR  + language + "_train_multiclass_labels_merged.pkl", "rb"))
         X_test=pickle.load(open(  OUTPUT_DIR  + language + "_X_test_ngrams.pkl", "rb"))
-        y_test=pickle.load(open(  OUTPUT_DIR  + language + "_test_multiclass_labels.pkl", "rb"))
+        y_test=pickle.load(open(  OUTPUT_DIR  + language + "_test_multiclass_labels_merged.pkl", "rb"))
         word_counts=pickle.load(open (OUTPUT_DIR + language + "_wc.pkl", "rb"))
-#     clean_labels=import_clean_labels("/Users/fabon.dzogang/projects/perso/chatbots-intent/chatbot-intents/multiclass/translate/spanish/round2_denoise/denoise_%s_labels.txt" % language)
     clean_labels=import_clean_labels("/Users/fabon.dzogang/projects/perso/chatbots-intent/chatbot-intents/multiclass/translate/%s/denoise_%s_labels.txt" % (language,language))
     clean_labels=np.array(clean_labels)
-
-    y_train=(np.array(y_train)[training_inds]).tolist()
+    if training_inds is not None:
+        y_train=(np.array(y_train)[training_inds]).tolist()
     print (X_train.shape)
     print (len(y_train))
     print (X_test.shape)
@@ -114,31 +113,31 @@ def train_model(recompute_index, training_inds, language, trial):
     import sklearn
 
     history=model.fit(X_train, to_categorical(y_train), batch_size=32, epochs=20, shuffle=True, class_weight=class_weights)
-    pickle.dump(model, open(OUTPUT_DIR + language + "_ngrams_model_%i_%i.pkl" % (trial, len(training_inds)), "wb"))
-    pickle.dump(history, open(OUTPUT_DIR + language + "_ngrams_history_%i_%i.pkl" % (trial, len(training_inds)), "wb"))
+    pickle.dump(model, open(OUTPUT_DIR + language + "_ngrams_model_%i_%i.pkl" % (trial, X_train.shape[0]), "wb"))
+    pickle.dump(history, open(OUTPUT_DIR + language + "_ngrams_history_%i_%i.pkl" % (trial, X_train.shape[0]), "wb"))
 
     return model, X_train, y_train, X_test, y_test, clean_labels
 
 def main(argv):
     data=[]
-    final_acc_scores=[]
 #     nb_trials=100
     import scipy.stats
 
     f=open("replication_ngrams_%s.csv" % argv[1], "w")
     f.write("sample_size,train_f1,train_prec,train_rec,test_f1,test_prec,test_rec,clean_f1,clean_prec,clean_rec\n")
-
     language=argv[1]
     training_sets=import_training_sets(language)
+    training_sets[1000000]=[1000000]
     training_sets=collections.OrderedDict(sorted(training_sets.items(), key=lambda tup:tup[0]))
     print_buff=[]
     for sample_size in training_sets.keys():
         print ("%i samples" % sample_size)
         training_set=training_sets[sample_size]
         nb_trials=10
+        final_acc_scores=[]
         for i in range(nb_trials):
             max_f1=[0]
-            model, X_train, y_train, X_test, y_test, clean_labels=train_model(i==0,training_set, language, i)
+            model, X_train, y_train, X_test, y_test, clean_labels=train_model(i==0, training_set, language, i)
 #             model, X_train, y_train, X_test, y_test, clean_labels=load_english_model(i==0, training_set, i)
             train_f1, train_prec, train_rec, test_f1, test_prec, test_rec, clean_f1, clean_prec, clean_rec=test_model(model, X_train, y_train, X_test, y_test, clean_labels, max_f1)
             data.append((train_f1, train_prec, train_rec, test_f1, test_prec, test_rec, clean_f1, clean_prec, clean_rec))
@@ -155,7 +154,7 @@ def main(argv):
         print_buff.append("MED [%.2f]" % (100*np.median(final_acc_scores)))
         print_buff.append("MIN [%.2f]" % (100*np.min(final_acc_scores)))
         print_buff.append("MAX [%.2f]" % (100*np.max(final_acc_scores)))
-        print_buff.append("IQR [%.4f]" % scipy.stats.iqr(final_acc_scores))
+        print_buff.append("IQR [%.2f]" % (100*scipy.stats.iqr(final_acc_scores)))
         print_buff.append("")
 
         print ("\n".join(print_buff[-9:]))
